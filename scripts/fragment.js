311694dcfa6c9ca23ba0f1c641de7dae3876b36c
BirdmanRS <34692344+BirdmanRS@users.noreply.github.com>
2025-10-20 16:22:16 -0500

    Fragment back to 1 Livery module

--- scripts/fragment.js
+++ scripts/fragment.js
@@ -24,65 +24,69 @@ const execute = async () => {
     // dir.closeSync();
 
     // NEW: Group aircraft by the model type in the directory name
-    const dir = fs.opendirSync(simObjectsBaseDir);
-    let dirent;
-    const groupedAircraft = {};
+    // const dir = fs.opendirSync(simObjectsBaseDir);
+    // let dirent;
+    // const groupedAircraft = {};
 
-    while ((dirent = dir.readSync()) !== null) {
-      const dirName = dirent.name;
-      // Ensure the directory name is long enough
-      if (dirName.length >= 10) {
-        const underscoreIndices = [];
-        for (let i = 0; i < dirName.length; i++) {
-          if (dirName[i] === "_") underscoreIndices.push(i);
-        }
-        let groupKey;
-        if (underscoreIndices.length >= 2) {
-          groupKey = dirName.substring(underscoreIndices[0] + 1, underscoreIndices[1]);
-        } else if (underscoreIndices.length === 1) {
-          groupKey = dirName.substring(underscoreIndices[0] + 1);
-        } else {
-          groupKey = dirName; // fallback if no underscores
-        }
-        groupKey = groupKey.toUpperCase();
-        if (!groupedAircraft[groupKey]) {
-          groupedAircraft[groupKey] = [];
-          console.log(`Creating group for ICAO: ${groupKey}`); // Log when a new group is created
-        }
-        groupedAircraft[groupKey].push({
-          name: dirName.replace("FSLTL_", ""),
-          sourceDir: `${simObjectsDir}${dirName}`,
-        });
-      }
-      else {
-        console.warn(`Directory name "${dirName}" is too short to extract ICAO code.`);
-      }
-    }
-    dir.closeSync();
+    // while ((dirent = dir.readSync()) !== null) {
+    //   const dirName = dirent.name;
+    //   // Ensure the directory name is long enough
+    //   if (dirName.length >= 10) {
+    //     const underscoreIndices = [];
+    //     for (let i = 0; i < dirName.length; i++) {
+    //       if (dirName[i] === "_") underscoreIndices.push(i);
+    //     }
+    //     let groupKey;
+    //     if (underscoreIndices.length >= 2) {
+    //       groupKey = dirName.substring(underscoreIndices[0] + 1, underscoreIndices[1]);
+    //     } else if (underscoreIndices.length === 1) {
+    //       groupKey = dirName.substring(underscoreIndices[0] + 1);
+    //     } else {
+    //       groupKey = dirName; // fallback if no underscores
+    //     }
+    //     groupKey = groupKey.toUpperCase();
+    //     if (!groupedAircraft[groupKey]) {
+    //       groupedAircraft[groupKey] = [];
+    //       console.log(`Creating group for ICAO: ${groupKey}`); // Log when a new group is created
+    //     }
+    //     groupedAircraft[groupKey].push({
+    //       name: dirName.replace("FSLTL_", ""),
+    //       sourceDir: `${simObjectsDir}${dirName}`,
+    //     });
+    //   }
+    //   else {
+    //     console.warn(`Directory name "${dirName}" is too short to extract ICAO code.`);
+    //   }
+    // }
+    // dir.closeSync();
 
-    // Add each grouped aircraft to aircraftobjects by each groupKey
-    Object.keys(groupedAircraft).forEach(groupKey => {
-      aircraftobjects.push({
-      name: groupKey,
-      sourceDir: groupedAircraft[groupKey].map(obj => obj.sourceDir),
-      });
-    });
+    // // Add each grouped aircraft to aircraftobjects by each groupKey
+    // Object.keys(groupedAircraft).forEach(groupKey => {
+    //   aircraftobjects.push({
+    //   name: groupKey,
+    //   sourceDir: groupedAircraft[groupKey].map(obj => obj.sourceDir),
+    //   });
+    // });
 
-    // This was added to create the draft 1.6 release... where all Lioveries are one large module
+    // This was added to create the draft 1.6 release... where all Liveries are one large module
     //
     // Liveries all in one assset
-    // aircraftobjects.push({
-    //   name: "Liveries",
-    //   sourceDir: simObjectsDir,
-    // });
+    aircraftobjects.push({
+      name: "Liveries",
+      sourceDir: simObjectsDir,
+    });
 
     aircraftobjects.push({
       name: "FLIGHTAWARE-FIX",
       sourceDir: "./AirTraffic",
     });
-    aircraftobjects.push({ name: "MISC", sourceDir: "./ModelBehaviorDefs" });
 
-    // Log to cnsole the aircraft objects for verification the sourceDirs are correct
+    aircraftobjects.push({
+      name: "MISC", 
+      sourceDir: "./ModelBehaviorDefs",
+    });
+
+    // Log to console the aircraft objects for verification the sourceDirs are correct
     aircraftobjects.forEach((obj, idx) => {
       console.log(`Aircraft Object ${idx + 1}:`, obj);
     });
