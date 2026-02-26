b4a94dd86cd6416ff4448adf885c050d273a5042
BirdmanRS <34692344+BirdmanRS@users.noreply.github.com>
2026-02-26 15:41:25 -0600

    A21N rebuild
    - added fan blade slow animations
    - added new lighting changes
    - renamed PW model files to be consistent
    Added MSR A359
    Rerun Stubgen
    Rerun VMRgen

--- scripts/VMR_generator.py
+++ scripts/VMR_generator.py
@@ -46,7 +46,11 @@ ModelsDirectories = [findCommunity()]
 # Configuration options
 ExcludeStubs = True   #whether to exclude models with 'STUB' in the title
 SameFamily = False  #whether to only use models from the same family for fallback
-ExcludeTypeBlank = True  #whether to exclude entries using cross-family airline liveries when exact-type ZZZZ exists (only applies when SameFamily is False)
+ExcludeTypeBlank = False  #whether to exclude entries using cross-family airline liveries when exact-type ZZZZ exists (only applies when SameFamily is False)
+
+# Option1 = SameFamily = True
+# Option2 = SameFamily = False, ExcludeTypeBlank = False
+# Option3 = SameFamily = False, ExcludeTypeBlank = True
 
 class Airplane:
   def __init__(self, TypeCode, Size, Manufacturer, EngineType, WideBody, neoExists, neo, Family):
